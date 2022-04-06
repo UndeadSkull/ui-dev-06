f4afea42c147cd27383114ff1a9ec3f5e0a08d1c
Muhammed Favas <favasme08@gmail.com>
2022-04-07 00:16:28 +0530

    Sticky header and some responsive fixes

--- index.js
+++ index.js
@@ -47,3 +47,10 @@ function sideScroll(element, delta, speed, distance) {
     }
   }, speed);
 }
+
+const observer = new IntersectionObserver( 
+  ([e]) => e.target.toggleAttribute('stuck', e.intersectionRatio < 1),
+  {threshold: [1]}
+);
+
+observer.observe(document.querySelector('header'));
