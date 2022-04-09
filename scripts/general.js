4b5b744336e90b7cd0005785da7b2edf3d58f255
Muhammed Favas <favasme08@gmail.com>
2022-04-09 15:07:00 +0530

    Cottages Page

--- scripts/general.js
+++ scripts/general.js
@@ -1,14 +1,11 @@
+// Sticky Header
 const observer = new IntersectionObserver(
     ([e]) => e.target.toggleAttribute('stuck', e.intersectionRatio < 1),
     { threshold: [1] }
 );
-
 observer.observe(document.querySelector('header'));
 
-function openNav() {
-    document.getElementById("mobile-nav").style.width = "300px";
-}
-
-function closeNav() {
-    document.getElementById("mobile-nav").style.width = "0";
-}
+// Mobile Nav
+const mobileNav = document.getElementById("mobile-nav")
+openNav = () => mobileNav.style.width = "300px"
+closeNav = () => mobileNav.style.width = "0"
--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,28 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-setInterval(function () {
-  plusSlides(1)
-}, 5000);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
+// Hero Section Slider
+showSlides = (n) => {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
+  let slides = document.getElementsByClassName("heroSlides");
+  if (n > slides.length) slideIndex = 1
+  if (n < 1) slideIndex = slides.length
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slides[slideIndex - 1].style.display = "block";
 }
 
-const scrollContainer1 = document.querySelector(".gallery-container");
-scrollContainer1.addEventListener("wheel", (evt) => {
-  evt.preventDefault();
-  sideScroll(scrollContainer1, evt.deltaY, 5, 300);
-});
+let slideIndex = 1;
+showSlides(slideIndex);
 
-const scrollContainer2 = document.querySelector(".facilities-container");
+setInterval(() => plusSlides(1), 5000)
+plusSlides = (n) => showSlides(slideIndex += n)
 
-function scrollRight() {
-  sideScroll(scrollContainer2, 300, 5, 320)
-}
-function scrollLeft() {
-  sideScroll(scrollContainer2, -300, 5, 320)
+// Booking Input Value
+inputChange = (elem) => {
+  document.querySelectorAll(".subtitle")[elem].innerHTML = document.querySelectorAll(".book-input")[elem].value
 }
 
-function sideScroll(element, delta, speed, distance) {
+// Gallery And Facilities Scroll
+sideScroll = (element, delta, speed, distance) => {
   step = delta / 50;
   scrollAmount = 0;
   var slideTimer = setInterval(function () {
@@ -47,6 +34,12 @@ function sideScroll(element, delta, speed, distance) {
   }, speed);
 }
 
-function dateChange(elem) {
-  document.querySelectorAll(".subtitle")[elem].innerHTML = document.querySelectorAll(".date")[elem].value
-}
+const scrollContainer1 = document.querySelector(".gallery-container");
+scrollContainer1.addEventListener("wheel", (evt) => {
+  evt.preventDefault();
+  sideScroll(scrollContainer1, evt.deltaY, 5, 300);
+});
+
+const scrollContainer2 = document.querySelector(".facilities-container");
+scrollRight = () => sideScroll(scrollContainer2, 300, 5, 320)
+scrollLeft = () => sideScroll(scrollContainer2, -300, 5, 320)
